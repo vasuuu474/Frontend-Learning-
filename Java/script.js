c5425cc0dcb3b4d28b15fad2a81d656c35c6f054
unknown <vasudevvk2006@gmail.com>
2026-06-26 17:52:02 +0530

    Learned js basics

--- Java/script.js
+++ Java/script.js
@@ -1,5 +1,37 @@
-const v = 64
+/*let name = 'Vasu'; //string literal
+let age = 19; // Number literal
+let isApproved = true ; //Boolean Literal
+let firstName ; 
+let lastName = null ; //Explicitly clear value of a variable
+*/
 
-console.log(`V = ${v}`)
-console.error("Error")
-console.warn("Warning")
+//object
+let person = {
+    name : 'Vasudev ' ,
+    age : 19
+}; 
+
+//Dot notation
+person.name = 'Vasu' ;
+
+//Bracket Notation
+let selection = 'name'
+person[selection] = 'Mary' ;
+
+console.log(person.name);
+
+//ARRAY 
+let selectedColors = ['red' , 'green' , 'blue'];
+selectedColors[3] = 'yellow'
+console.log(selectedColors);
+
+//functions
+function greet(firstName , lastName ){
+    console.log('HELLO ' + firstName + ' ' + lastName);
+}
+greet('Vasudev' , 'VK');
+
+function sum(num1 , num2){
+    return num1 + num2;
+}
+console.log('Sum = ' + sum(1 , 1));
